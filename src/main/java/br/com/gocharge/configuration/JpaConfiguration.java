207cc207851e17d97969616ad950deee8edc62c8
Marcos Tischer Vallim <tischer@gmail.com>
2021-03-18 21:07:38 -0300

    Changes:
    * Initial version

--- src/main/java/br/com/gocharge/configuration/JpaConfiguration.java
+++ src/main/java/br/com/gocharge/configuration/JpaConfiguration.java
@@ -1,6 +1,5 @@
 package br.com.gocharge.configuration;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -8,6 +7,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableTransactionManagement
 @EntityScan(basePackages = "br.com.gocharge.model")
-@EnableAutoConfiguration(exclude = DataSourceConfiguration.class)
 class JpaConfiguration {
+
 }
--- src/main/java/br/com/gocharge/configuration/LocalDataBaseConfiguration.java
+++ src/main/java/br/com/gocharge/configuration/LocalDataBaseConfiguration.java
@@ -3,28 +3,24 @@ package br.com.gocharge.configuration;
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.apache.tomcat.jdbc.pool.PoolProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @Profile("dev")
 @ConditionalOnProperty(value = "h2.enabled", havingValue = "true")
-@EnableTransactionManagement
-@EnableJpaRepositories(basePackages = "br.com.gocharge.repository")
-@EntityScan(basePackages = "br.com.gocharge.model")
 class LocalDataBaseConfiguration {
 
   @Bean
   @ConfigurationProperties("app.h2.datasource")
-  public PoolProperties dataSourceProperties() {return new PoolProperties(); }
+  public PoolProperties dataSourceProperties() {
+    return new PoolProperties();
+  }
 
   @Bean
-  public DataSource dataSource(PoolProperties poolProperties) {
+  public DataSource dataSource(final PoolProperties poolProperties) {
     final DataSource dataSource = new DataSource();
 
     dataSource.setUrl(poolProperties.getUrl());
