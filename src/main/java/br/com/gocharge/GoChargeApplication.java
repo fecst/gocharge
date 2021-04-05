a6f02ad925a994eae167434bac22b9c27870df24
costinha <fcosta.paltrinieri@gmail.com>
2021-04-05 19:08:49 -0300

    Build JAR

--- src/main/java/br/com/gocharge/GoChargeApplication.java
+++ src/main/java/br/com/gocharge/GoChargeApplication.java
@@ -3,10 +3,10 @@ package br.com.gocharge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackageClasses = GochargeApplication.class)
-public class GochargeApplication {
+@SpringBootApplication(scanBasePackageClasses = GoChargeApplication.class)
+public class GoChargeApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(GochargeApplication.class, args);
+    SpringApplication.run(GoChargeApplication.class, args);
   }
 }
